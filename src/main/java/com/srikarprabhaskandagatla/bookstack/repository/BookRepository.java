f4cca8c142177a073eb4c9c87f084734686762b9
srikarprabhaskandagatla <skandagatla@umass.edu>
2025-06-14 16:24:40 -0500

    'books' Table Interface

---
+++ src/main/java/com/srikarprabhaskandagatla/bookstack/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.srikarprabhaskandagatla.bookstack.repository;
+
+import com.srikarprabhaskandagatla.bookstack.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {}
