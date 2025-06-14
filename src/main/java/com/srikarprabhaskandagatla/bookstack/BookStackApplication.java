6ee6c3dc99fd0c087aa9b7d4a6bce45cc3078f7a
srikarprabhaskandagatla <skandagatla@umass.edu>
2025-06-14 13:52:25 -0500

    Add Initial Resources

---
+++ src/main/java/com/srikarprabhaskandagatla/bookstack/BookStackApplication.java
@@ -0,0 +1,13 @@
+package com.srikarprabhaskandagatla.bookstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStackApplication.class, args);
+	}
+
+}
