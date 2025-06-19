3a4a72c5bea4c485bb6329e5ef8c6b648a527dfd
srikarprabhaskandagatla <skandagatla@umass.edu>
2025-06-18 21:25:59 -0500

    Redundant Function

--- src/main/java/com/srikarprabhaskandagatla/bookstack/model/Book.java
+++ src/main/java/com/srikarprabhaskandagatla/bookstack/model/Book.java
@@ -10,7 +10,7 @@ public class Book {
     private String title;
     private int quantity;
 
-    public Book() {}
+    // public Book() {}
 
     public Book(String title, int quantity) {
         this.title = title;
