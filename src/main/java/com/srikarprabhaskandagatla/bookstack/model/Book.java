775b67afdf4f59df94af15ada82360541d1e51b7
srikarprabhaskandagatla <skandagatla@umass.edu>
2025-06-20 23:59:44 -0500

    Update

--- src/main/java/com/srikarprabhaskandagatla/bookstack/model/Book.java
+++ src/main/java/com/srikarprabhaskandagatla/bookstack/model/Book.java
@@ -10,7 +10,7 @@ public class Book {
     private String title;
     private int quantity;
 
-    public Book() {}
+    public Book() {} // Book Stack Constructor
 
     public Book(String title, int quantity) {
         this.title = title;
